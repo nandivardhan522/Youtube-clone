558dfc6429b240b9a53a028086f99f099f45a5c0
Nandivardhan Reddy <nandivardhanreddybhumireddy@gmail.com>
2024-12-07 16:15:19 -0600

    Video Processing

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,13 +1,30 @@
 import express from "express";
+import ffmpeg from "fluent-ffmpeg";
 
-const app=express();
-const port=3000;
+const app = express();
+app.use(express.json());
 
-app.get("/",(req,res) =>{
-    res.send("Hello World");
+app.post("/process-video",(req,res)=>{
+    const inputFilePath = req.body.inputFilePath;
+    const outputFilePath = req.body.outputFilePath;
+    console.log("Received payload:", req.body);
+    if (!inputFilePath || !outputFilePath) {
+        res.status(400).send("Bad Request: Missing file path.");
+      }
+
+  ffmpeg(inputFilePath)
+    .outputOptions("-vf", "scale=-1:360") // 360p
+    .on("end", () => {
+      res.status(200).send("Processing Finished successfully.");
+    })
+    .on("error", (err) => {
+      console.error(`An error occurred: ${err.message}`);
+      res.status(500).send(`Internal Server Error: ${err.message}`);
+    })
+    .save(outputFilePath);
 });
 
-app.listen(port,()=>{
-    console.log(
-        'Video processing service is listening at http://localhost:${port}')
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Video processing service is listening at http://localhost:${port}`);
 });
